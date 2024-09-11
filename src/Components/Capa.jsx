a97103d15e935ed789a056391d283379ca1fa7b4
Luiza Oyara <oyaraluiza@gmail.com>
2024-09-11 20:17:16 -0300

    update

--- src/Components/Capa.jsx
+++ src/Components/Capa.jsx
@@ -8,19 +8,19 @@ const Capa = () => {
     return (
         <div style={{backgroundColor: 'white', color: 'black', textAlign: 'center', border: '2px solid black', padding: '20px', borderRadius: '10px', height: '80vh'}}>
 
-            <h1 style={{fontSize: '45px', fontFamily: 'initial', width:'200px', position:'absolute', top:'145px', right:'43%'}}>Luiza Oyara</h1>
+            <h1 style={{ fontSize: '45px', fontFamily: 'initial', width:'300px', position:'absolute', top:'170px', right:'40%'}}>Luiza Oyara</h1>
 
             <h2 style={{fontFamily:'cursive', fontSize: '40px', width: '200px', position: 'absolute', top: '250px', right: '43%' }}>Portifólio</h2>
 
             <nav>
-                <ul style={{textAlign: 'justify', marginTop: '10%', }}>
+                <ul style={{textAlign: 'justify', marginTop: '10%', top: '175px', position:"absolute" }}>
                     <li>Empresa - Sicoob</li>
                     <li>Programa - CIEE Florianópolis</li>
                     <li>Supervisor - Gelson</li>
                 </ul>
             </nav>
             
-            <h2 style={{width: '300px', position: 'absolute', top:'400px', right: '40%'}}>Breve relato sobre os 10 dias iniciais</h2>
+            <h2 style={{fontSize:'35px', width: '350px', position: 'absolute', top:'380px', right: '38%'}}>Breve relato sobre os 10 dias iniciais</h2>
 
             <img src={Ciee} alt="ciee" style={{width: '200px', position: 'absolute', top: '500px', right: '80%' }}/>
 
