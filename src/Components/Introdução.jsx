1d582b7e17c14b1b954f120e5bc5415b91bfaa9e
Luiza Oyara <oyaraluiza@gmail.com>
2024-09-12 15:42:01 -0300

    ii

--- src/Components/Introdução.jsx
+++ src/Components/Introdução.jsx
@@ -7,12 +7,13 @@ const Introdução = () => {
 
             <div style={{backgroundColor: 'rgb(6, 82, 82)', width:'50%', border: '2px solid black', borderRadius: '10px'}}>
             <h1>O Que É Um Portifólio?</h1>
-            <p style={{fontSize:'30px', width:'400px',  top:'650px', right:'10%'}}>Esse portifólio, em específico, é uma atividade que reúne os principais registros dos encontros online do Programa CIEE, a fim de mostrar para as empresas o que nos foi ensinado.</p>
+            <p style={{fontSize:'30px', width:'350px',  top:'650px', right:'15%'}}>Esse portifólio, em específico, é uma atividade que reúne os principais registros dos encontros online do Programa CIEE, a fim de mostrar para as empresas o que nos foi ensinado.</p>
             </div>
 
-            <div>
-                <h1 style={{color:'white'}}>oi</h1>
-            <img src={Portifolio} alt="portifolio" style={{width: '300px', position:'absolute', right:'10%', height:'550px', top:'645px', borderRadius:'5px'}}/>
+            <div style={{backgroundColor: '#01B09F', width:'50%', border: '2px solid black', borderRadius: '10px' }}>
+                <h1 style={{color:'white', right:'40%', width:'400px'}}>Quem Sou Eu?</h1>
+                <p></p>
+            <img src={Portifolio} alt="portifolio" style={{width: '300px', position:'absolute', right:'0%', height:'550px', top:'645px', borderRadius:'5px',margin:'2px 2px'}}/>
             </div>
             
         </div>
