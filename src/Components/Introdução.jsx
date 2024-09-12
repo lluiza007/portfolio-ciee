60d118acef3686754d339688e4b3ec477c53a819
Luiza Oyara <oyaraluiza@gmail.com>
2024-09-12 15:06:47 -0300

    hi

--- src/Components/Introdução.jsx
+++ src/Components/Introdução.jsx
@@ -3,12 +3,17 @@ import Portifolio from '../img/portifolio.png'
 
 const Introdução = () => {
     return (
-        <div style={{backgroundColor: 'rgb(6, 82, 82)', color: 'white', textAlign: 'center', border: '2px solid black', padding: '0', borderRadius: '10px', margin:'5px 0', height: '550px', width: '50%'}}>
+        <div style={{ color: 'white', textAlign: 'center', border: '2px solid black', padding: '0', borderRadius: '10px', margin:'5px 0', height: '550px', display:'flex'}}>
 
+            <div style={{backgroundColor: 'rgb(6, 82, 82)', width:'50%'}}>
             <h1>O Que É Um Portifólio?</h1>
             <p style={{fontSize:'30px', width:'400px',  top:'650px', right:'10%'}}>Esse portifólio, em específico, é uma atividade que reúne os principais registros dos encontros online do Programa CIEE, a fim de mostrar para as empresas o que nos foi ensinado.</p>
+            </div>
 
+            <div>
+                <h1 style={{color:'white'}}>oi</h1>
             <img src={Portifolio} alt="portifolio" style={{width: '300px', position:'absolute', right:'10%', height:'550px', top:'645px', borderRadius:'5px'}}/>
+            </div>
             
         </div>
     )
