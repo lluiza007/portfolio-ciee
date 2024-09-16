d025df55a05d4fbde0049cd13ea1d1bdea778c97
Luiza Oyara <oyaraluiza@gmail.com>
2024-09-16 14:45:13 -0300

    e

--- src/Components/Introdução.jsx
+++ src/Components/Introdução.jsx
@@ -1,27 +1,22 @@
 import React from "react";
-import Portifolio from '../img/portifolio.png'
-import "./Introdução.css"
+import Portifolio from '../img/portifolio.png';
+import "./Introdução.css";
 
-const Introdução = () => {
+const Introducao = () => {
     return (
         <div className="container">
-
             <div className="nome">
-            <h1>O Que É O Portifólio CIEE?</h1>
-            <b>
-            <p className="p">Esse portifólio, em específico, é uma atividade que reúne os principais registros dos encontros online do Programa CIEE, a fim de mostrar para as empresas o que nos foi ensinado.</p></b>
+                <h1>O Que É O Portfólio CIEE?</h1>
+                <p className="p">Esse portfólio, em específico, é uma atividade que reúne os principais registros dos encontros online do Programa CIEE, a fim de mostrar para as empresas o que nos foi ensinado.</p>
             </div>
-           
-          
 
             <div className="div">
                 <h1 className="h1">Quem Sou Eu?</h1>
                 <p>Olá, me chamo Luiza Oyara Garcia Abreu, tenho 17 anos e estou atualmente cursando o último ano do ensino médio.</p>
-            <img src={Portifolio} alt="portifolio" className="img"/>
+                <img src={Portifolio} alt="portifolio" className="img" />
             </div>
-            
         </div>
     )
 }
 
-export default Introdução;
+export default Introducao;
