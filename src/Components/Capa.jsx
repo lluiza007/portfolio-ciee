a26c6fb84fdb11077ccb784faa25baab7b864ed0
Luiza Oyara <oyaraluiza@gmail.com>
2024-09-16 14:10:08 -0300

    fucked up

--- src/Components/Capa.jsx
+++ src/Components/Capa.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import Ciee from '../img/ciee.png';
 import Sicoob from '../img/sicoob.png';
+import "./Capa.css";
 
 
 
 const Capa = () => {
     return (
-        <div style={{backgroundColor: 'white', color: 'black', textAlign: 'center', border: '2px solid black', padding: '20px', borderRadius: '10px', height: '80vh'}}>
+        <div className="container">
 
-            <h1 style={{ fontSize: '45px', fontFamily: 'initial', width:'300px', position:'absolute', top:'170px', right:'40%'}}>Luiza Oyara</h1>
+            <h1 className="nome">Luiza Oyara</h1>
 
-            <h2 style={{fontFamily:'cursive', fontSize: '40px', width: '200px', position: 'absolute', top: '250px', right: '43%' }}>Portifólio</h2>
+            <h2 className="portifolio">Portifólio</h2>
 
             <b>
             <nav>
-                <ul style={{textAlign: 'justify', marginTop: '10%', top: '175px', position:"absolute" }}>
+                <ul className="nav">
                     <li>Empresa - Sicoob</li>
                     <li>Programa - CIEE Florianópolis</li>
                     <li>Supervisor - Gelson</li>
@@ -22,7 +23,7 @@ const Capa = () => {
             </nav>
             </b>
             
-            <h2 style={{fontSize:'35px', width: '350px', position: 'absolute', top:'380px', right: '38%'}}>Breve relato sobre os 10 dias iniciais</h2>
+            <h2 className="h2">Breve relato sobre os 10 dias iniciais</h2>
 
             <img src={Ciee} alt="ciee" style={{width: '200px', position: 'absolute', top: '500px', right: '85%' }}/>
 
--- src/Components/Introdução.jsx
+++ src/Components/Introdução.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import Portifolio from '../img/portifolio.png'
+import "./Introdução.css"
 
 const Introdução = () => {
     return (
-        <div style={{ color: 'white', textAlign: 'center', border: '2px solid black', padding: '0', borderRadius: '10px', margin:'5px 0', height: '550px', display:'flex'}}>
+        <div className="container">
 
-            <div style={{backgroundColor: '#00665C', width:'50%', border: '2px solid black', borderRadius: '10px'}}>
+            <div className="nome">
             <h1>O Que É O Portifólio CIEE?</h1>
             <b>
-            <p style={{fontSize:'25px', color:'black'}}>Esse portifólio, em específico, é uma atividade que reúne os principais registros dos encontros online do Programa CIEE, a fim de mostrar para as empresas o que nos foi ensinado.</p></b>
+            <p className="p">Esse portifólio, em específico, é uma atividade que reúne os principais registros dos encontros online do Programa CIEE, a fim de mostrar para as empresas o que nos foi ensinado.</p></b>
             </div>
            
           
 
-            <div style={{backgroundColor: '#01B09F', width:'50%', border: '2px solid black', borderRadius: '10px' }}>
-                <h1 style={{color:'white', right:'40%', width:'400px'}}>Quem Sou Eu?</h1>
+            <div className="div">
+                <h1 className="h1">Quem Sou Eu?</h1>
                 <p>Olá, me chamo Luiza Oyara Garcia Abreu, tenho 17 anos e estou atualmente cursando o último ano do ensino médio.</p>
-            <img src={Portifolio} alt="portifolio" style={{width: '300px', position:'absolute', right:'0%', height:'550px', top:'645px', borderRadius:'5px',margin:'2px 2px'}}/>
+            <img src={Portifolio} alt="portifolio" className="img"/>
             </div>
             
         </div>
