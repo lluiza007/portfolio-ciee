4fe2957a3e8438934c5d4788d610302efc4219bb
Luiza Oyara <oyaraluiza@gmail.com>
2024-09-16 19:10:23 -0300

    a

--- src/Components/Conclusão.jsx
+++ src/Components/Conclusão.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import '../Styles/Conclusão.css';
 
 const Conclusão = () => {
     return (
-        <div style={{backgroundColor: 'white', color: 'black', textAlign: 'center', border: '2px solid black', padding: '20px', borderRadius: '10px', height: '50vh'}}>
+        <div className="conc">
         
         <h1>Conclusão</h1>
+        <p>Em resumo, ao longo desses 10 dias, pude me aprofundar nos temas trabalhados(muitos já tinha visto, porém, não tinha me aprofundado antes), adquirindo conhecimentos que com certeza me ajudaram a evoluir tanto no âmbito profissional,como pessoal.</p>
 
         </div>
     )
--- src/Components/SegundoEnc.jsx
+++ src/Components/SegundoEnc.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import '../Styles/SegundoEnc.css';
 
 const SegundoEnc = () => {
     return (
-        <div style={{backgroundColor: 'rgb(6, 82, 82)', color: 'black', textAlign: 'center', border: '2px solid black', padding: '0', borderRadius: '10px', margin:'5px 0', height: '400px'}}>
+        <div className="segundo">
             
         </div>
         
