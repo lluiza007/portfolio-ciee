dd3c80a7e4e1985c363ed98417b2df2f31c69459
Luiza Oyara <oyaraluiza@gmail.com>
2024-09-17 19:05:44 -0300

    quase lá

--- src/Components/PrimeiroEnc.jsx
+++ src/Components/PrimeiroEnc.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import '../Styles/PrimeiroEnc.css';
+import Eca from '../img/Eca.png';
 
 const PrimeiroEnc = () => {
     return (
         <div className="primeiro">
+            <br></br>
 
             <h2>Leis Da Aprendizagem - 09/09/2024</h2>
-            <p>No 1<sup>0</sup> encontro, fomos apresentados em uma videoconferência a tutoriais com a professora Thaís e depois as Leis de Aprendizagem. </p>
+            <br></br>
 
-            <h3>Conferência</h3>
-            <p>Começamos com uma videoconferência com a tutora Thais, onde ela nos auxiliou e explicou como acessar a plataforma do ciee e como fazer as atividades.</p>
+            <p>No 1<sup>0</sup> encontro, fomos apresentados em uma videoconferência a tutoriais com a professora Thaís e depois as Leis de Aprendizagem. </p>
+            <br></br>
             
             <h3>Lei da Aprendizagem</h3>
-            <p>É uma lei no Brasil que determina que empresas de médio e grande porte contratemjovens entre 14 e 24 anos como aprendizes. Nessa lei é falado sobre o que e como os jovens devem trabalhar, sempre conciliando os estudos com o trabalho, com a carga horária dividida entre atividades práticas na empresa e teorícas nas instituições de ensino.</p>
+            <br></br>
+            <p>É uma lei no Brasil que determina que empresas de médio e grande porte contratemjovens entre 14 e 24 anos como aprendizes. Nessa lei é falado sobre o que e como os jovens devem trabalhar, sempre conciliando os estudos com o trabalho, com a carga horária dividida entre atividades práticas na empresa e teorícas nas instituições de ensino. Aprendemos também sobre o ECA (instituto da criança e do adolescente), e como ele foi importante para a regulamentação do que conhecemos hoje como jovem aprendiz.</p>
+
+            <img src={Eca} alt="eca" className="eca" />
         </div>
         
     )
--- src/Components/SegundoEnc.jsx
+++ src/Components/SegundoEnc.jsx
@@ -4,9 +4,42 @@ import '../Styles/SegundoEnc.css';
 const SegundoEnc = () => {
     return (
         <div className="segundo">
-            <h1>bla</h1>
+            <br></br>
+            <h1>Direitos e Deveres do Empregado e Empregador</h1>
+            <br></br>
+            <p>Os direitos e deveres do trabalhador e do empregador são regulados pela Consolidação das Leis do Trabalho (CLT) no Brasil. Ambos têm responsabilidades mútuas que visam garantir um ambiente de trabalho justo e produtivo.</p>
+            <br></br>
+
+            <div className="empregado">
+                <h2>Direitos e deveres do empregado:</h2>
+                <br></br>
+                <ul className="ul">
+                    <li>Cumprir diretrizes;</li>
+                    <br></br>
+                    <li>Ser ético;</li>
+                    <br></br>
+                    <li>Ser pontual;</li>
+                    <br></br>
+                    <li>Ser responsável;</li>
+                    <br></br>
+                    <li>Receber fgts;</li>
+                    <br></br>
+                    <li>Descanso durante o expediente;</li>
+                    <br></br>
+                    <li>Férias;</li>
+                    <br></br>
+                    <li>Remuneração;</li>
+                </ul>
+
+            </div>
+            <div className="empregador">
+            <h2>Direitos e deveres do empregador:</h2>
+
+
+            </div>
             
         </div>
+       
         
     )
 }
