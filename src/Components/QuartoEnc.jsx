f0a1c4d5ff1e4a0d7f7204df09db04fd4bd9d0c8
Luiza Oyara <oyaraluiza@gmail.com>
2024-09-18 15:03:21 -0300

    almost

--- src/Components/QuartoEnc.jsx
+++ src/Components/QuartoEnc.jsx
@@ -8,6 +8,32 @@ const QuartoEnc = () => {
             <br></br>
             <h1 style={{fontSize:'40px', textAlign:'center'}}>Ética Profissional</h1>
             <h3 style={{textAlign:'center'}}>O 4<sup>o</sup> encontro nos mostrou sobre ética profissional</h3>
+            <br></br>
+            <p style={{textAlign:'center'}}>A ética profissional é o conjunto de princípios e valores que orientam o comportamento no ambiente de trabalho, garantindo atitudes corretas, respeitosas e justas. Ela envolve a responsabilidade com as tarefas, o respeito às normas e aos colegas, além da transparência nas ações. Seguir uma conduta ética é essencial para construir relações de confiança, promover um ambiente saudável e evitar conflitos que possam prejudicar a reputação pessoal e da organização. No contexto profissional, a ética é a base para o sucesso e a credibilidade a longo prazo.</p>
+
+            <div className="asy">
+            <div className="yas">
+                <h2>Pontos Positivos:</h2>
+                <br></br>
+                <ul>
+                    <li>Melhora o clima organizacional;</li>
+                    <li>Melhora a produtividade;</li>
+                    <li>Valorização profissional;</li>
+                    <li>Agir de forma ética aumenta a confiança de colaboradores;</li>
+                </ul>
+            </div>
+
+            <div className="say">
+                <h2>Características:</h2>
+                <ul>
+                    <li>Honestidade;</li>
+                    <li>Respeito;</li>
+                    <li>Comunicação;</li>
+                    <li>Honestidade;</li>
+                    <li>Honestidade;</li>
+                </ul>
+            </div>
+            </div>
         </div>
     )
 }    
