1ae5f3239b9d145236351c073bb865cbeb1f3091
Luiza Oyara <oyaraluiza@gmail.com>
2024-09-18 14:31:35 -0300

    huh

--- src/Components/QuartoEnc.jsx
+++ src/Components/QuartoEnc.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import '../Styles/QuartoEnc.css';
 
 
 const QuartoEnc = () => {
     return (
         <div className="quarto">
-            <h1>as</h1>
+            <br></br>
+            <h1 style={{fontSize:'40px', textAlign:'center'}}>Ética Profissional</h1>
+            <h3 style={{textAlign:'center'}}>O 4<sup>o</sup> encontro nos mostrou sobre ética profissional</h3>
         </div>
     )
 }    
--- src/Components/TerceiroEnc.jsx
+++ src/Components/TerceiroEnc.jsx
@@ -6,11 +6,17 @@ const TerceiroEnc = () => {
     return (
         <div className="terceiro">
             <br></br>
-            <h1 style={{fontSize: '40px'}}>Conceitos e Tipos de Empresa</h1>
-            <h3>O 3 <sup>0</sup> encontro ensinou sobre os perfis de empresa</h3>
+            <h1 style={{fontSize: '40px', textAlign: 'center'}}>Conceitos e Tipos de Empresa</h1>
+            <h3 style={{textAlign: 'center'}}>O 3<sup>o</sup> encontro ensinou sobre os perfis de empresa</h3>
+            <h3 style={{textAlign: 'center'}}>-histórico, missão, visão, valores, hierarquia e organograma-</h3>
+            <h2 style={{marginLeft:'100px', marginTop: '100px'}}>Organograma Sicoob</h2>
+        
             <p></p>
             
+            
+           
             <img src={Organograma} alt="org" className="org"/>
+            
         </div>
         
     )
