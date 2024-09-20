a0e2885497b79914d3bd36da1476cbe09380a7da
Luiza Oyara <oyaraluiza@gmail.com>
2024-09-20 12:25:29 -0300

    aa

--- src/Components/OitavoEnc.jsx
+++ src/Components/OitavoEnc.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 import '../Styles/Oitavo.css';
+import Computador from '../img/computador.jpg';
 const OitavoEnc = () => {
     return (
         <div className="oitavo">
             <h1 style={{textAlign:'center', fontFamily:'40px'}}>Noções Básicas do Computador</h1>
+            <h3 style={{textAlign:'center'}}>No 8<sup>o</sup> encontro, falamos sobre computaçaõ, algumas noções básicas, etc.</h3>
+            <br></br>
+
+            <div style={{width:'50%', float:'right',}}>
+                <img src={Computador} alt="com" style={{width:'250px'}} />
+            </div>
+            <div style={{width:'50%', marginTop:'50px'}}>
+                <h2>Qual o significado de tecnologia?</h2>
+                <br></br>
+                <p style={{textAlign:'left', marginLeft:'20px'}}>A tecnologia pode ser definida como o conjunto de conhecimentos, técnicas, métodos e processos utilizados para criar ferramentas, máquinas, dispositivos e sistemas que resolvem problemas e facilitam a vida humana. Ela envolve a aplicação prática da ciência para o desenvolvimento de soluções que melhoram o dia a dia, desde as mais simples, como o uso de ferramentas manuais, até as mais complexas, como computadores, inteligência artificial e redes de comunicação.</p>
+                <br></br>
+
+                <h3 >A tecnologia ajuda as empresas em diversos setores:</h3>
+                <br></br>
+                <ul style={{textAlign:'left', marginLeft:'100px'}}>
+                    <li>Recursos humanos: coleta e seleção de documentos;</li>
+                    <li>Produção: mensura os resultados de desempenho por equipe e individualmente</li>
+                </ul>
+            </div>
+           
         </div>
     )
 }
--- src/Components/SetimoEnc.jsx
+++ src/Components/SetimoEnc.jsx
@@ -10,7 +10,7 @@ const SetimoEnc = () => {
             <p style={{textAlign:'center'}}>A Saúde do Trabalhador é o conjunto de atividades do campo da saúde coletiva que se destina, por meio das ações de vigilância epidemiológica e vigilância sanitária, à promoção e proteção da saúde dos trabalhadores, assim como visa à recuperação e reabilitação da saúde dos trabalhadores submetidos aos riscos e agravos advindos das condições de trabalho.</p>
             <br></br>
 
-            <div style={{marginLeft:'30px'}}>
+            <div style={{marginLeft:'30px', width:'50%'}}>
                 <h2>As 5 Principais Regras da NR 17:</h2>
                 <ul>
                     <li>Ergonomia; </li>
@@ -18,6 +18,8 @@ const SetimoEnc = () => {
                     <li>Transporte de cargas;</li>
                     <li>trabalhos manuais;</li>
                     <li>Organização do trabalho.</li>
+                    <br></br>
+                    <h3>O não cumprimento da NR 17 pode gerar multas e penalizações para a empresa que não cumprir as regras.</h3>
                 </ul>
             </div>
         </div>
